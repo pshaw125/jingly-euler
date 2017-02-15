f7fca0fffdfc82fa05df337089db10c5c19a8b4e
Peter Shaw <pshaw@microsoft.com>
2017-02-15 09:47:09 -0800

    Solve problem 48

---
+++ src/test/java/com/rhejinald/euler/problems/Problem48.java
@@ -0,0 +1,34 @@
+package com.rhejinald.euler.problems;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ *
+ * Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+ * -----------------------------------------
+ * Notes:
+ * This sounds scary, but it's not so much depending on the the jvm's ability to easily compute 999^999;
+ * If it sucks / takes ages, we can perform a more iterative "multiply then mod" strategy to keep the
+ * digit count down.
+ *
+ * Okay, so it finished in 11ms; I think this will be fine.
+ *
+ * Simple loop adding the powers; substring.
+ *
+ * Attempt 1: 9110846700 - Correct! 46ms,  85048th person to have solved this problem.
+ *
+ */
+public class Problem48 {
+    @Test
+    public void problem48() throws Exception {
+        BigDecimal bigDecimal = BigDecimal.valueOf(1);
+        for (int i = 2; i <= 1000; i++) {
+            bigDecimal = bigDecimal.add((BigDecimal.valueOf(i).pow(i)));
+        }
+        String s = bigDecimal.toString();
+        System.out.println(s.substring(s.length()-10));
+    }
+}
