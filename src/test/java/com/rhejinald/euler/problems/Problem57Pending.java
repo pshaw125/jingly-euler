0dc830ce445ad3e489d76b5b8c11d45dcc8c75ab
Peter Shaw <pshaw@microsoft.com>
2018-03-09 17:34:55 -0800

    Problem 57 -> pending

---
+++ src/test/java/com/rhejinald/euler/problems/Problem57Pending.java
@@ -0,0 +1,67 @@
+package com.rhejinald.euler.problems;
+
+import com.rhejinald.euler.lib.NumbersAsStrings;
+import org.apache.commons.lang3.math.Fraction;
+import org.apache.commons.math3.fraction.BigFraction;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+ * <p>
+ * √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+ * <p>
+ * By expanding this for the first four iterations, we get:
+ * <p>
+ * 1 + 1/2 = 3/2 = 1.5
+ * 1 + 1/(2 + 1/2) = 7/5 = 1.4
+ * 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+ * 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+ * <p>
+ * The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example
+ * where the number of digits in the numerator exceeds the number of digits in the denominator.
+ * <p>
+ * In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+ * <p>
+ * =================================
+ * Notes
+ * <p>
+ * So we don't actually need to worry about double type precision if we expand the fractions ourselves.
+ * Apache commons math handles the reduce function for finding lowest common denominators.
+ * The result of my first pass is actually the same whether or not the reduce function is in there, so I'm not even
+ * sure it makes a difference.
+ *
+ * The fractions represented as numbers actually really quickly overflowed what a long could handle.
+ *
+ * Anyway:
+ * Attempt 1 - 153 - ______
+ */
+public class Problem57Pending {
+
+    @Test
+    public void testProblem57OverflowSafe() throws Exception {
+        BigFraction fraction = new BigFraction(1, 2);
+        int count = 0;
+
+        int numberOfExpansions = 1000;
+        for (int i = 1; i < numberOfExpansions; i++) {
+            fraction = new BigFraction(1).divide(fraction.add(2)).reduce();
+            String denominator = fraction.getDenominator().toString();
+            String numeratorPlusOneWhole = fraction.getNumerator().add(fraction.getDenominator()).toString();
+            if(numeratorPlusOneWhole.length() > denominator.length()){
+                count++;
+            }
+        }
+        System.out.println("57>> final count of top-digit-heavy fractions: " + count);
+    }
+
+    @Test
+    public void testFractionLibWithNonSimplestForm() throws Exception {
+        assertThat(Fraction.getFraction("8/4").reduce().toString()).isEqualTo("2/1");
+        assertThat(new BigFraction(4, 16).reduce().toString()).isEqualTo("1 / 4");
+    }
+
+}
